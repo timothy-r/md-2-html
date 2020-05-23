c0ab1c8f89b3519c00ea0dd656995be2b08417d1
Tim Rodger <tim.rodger@gmail.com>
2020-05-23 13:16:11 +0100

    v1

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * take an md file as input
+ * produce a HTML version
+ */
+const path = require('path');
+
+const input = process.argv[2];
+
+// use the input file name without extension as the document title
+const items = path.parse(input);
+const docTitle = items['name'];
+
+const Markdown = require('markdown-to-html').Markdown;
+const md = new Markdown();
+md.bufmax = 2048;
+
+const opts = {title: docTitle, stylesheet: 'styles/styles.css'};
+
+// Write a header.
+//console.log('Starting...');
+
+// Write a trailer at eof.
+md.once('end', function() {
+  // console.log('Done...');
+});
+
+md.render(input, opts, function(err) {
+  if (err) {
+    console.error('>>> ' + err);
+    process.exit();
+  }
+  md.pipe(process.stdout);
+});
---
+++ test/page-1.md
@@ -0,0 +1,8 @@
+Test Page
+=========
+
+This is a paragraph.
+
+~ Here's an aside, with blank lines above and below
+
+"Quoted speech goes here"
